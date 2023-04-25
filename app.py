787760d9a26848db7d1465fc952c4b07e609a79d
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2023-04-25 23:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,11 @@ def pred():
 	age=int(request.form["age"])
 	sex=int(request.form["sex"])
 	cp=int(request.form["cp"])
-	trtbps=int(request.form["trtbps"])
-	chol=int(request.form["chol"])
-	fbs=int(request.form["fbs"])
-	restecg=int(request.form["restecg"])
 	thalachh=int(request.form["thalachh"])
-	exng=int(request.form["exng"])
-	oldpeak=float(request.form["oldpeak"])
-	slp=float(request.form["slp"])
-	caa=int(request.form["caa"])
 	thall=int(request.form["thall"])
 	with open("heart.model","rb") as f:
 		model=pickle.load(f)
-	d=[[age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall]]
+	d=[[age,sex,cp,thalachh,thall]]
 	res=model.predict(d)[0]
 	if res==0:	msg="Heart disease NOT detected"
 	elif res==1:	msg="Heart disease detected!!"
